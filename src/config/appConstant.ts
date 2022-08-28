916e59c42a0db248653db2f98ffb5ca8eb492685
Brajesh Pandey <pbrajesh79@gmail.com>
2022-08-28 18:52:58 +0000

    Constant changes for api

---
+++ src/config/appConstant.ts
@@ -0,0 +1,3 @@
+export const origin:string[] = ['http://localhost:3000','http://localhost:30001/','https://3001-brajeshpand-udharbacken-ojo69k8u80t.ws-us63.gitpod.io/']
+
+export const PORT = process.env.PORT || 3001;
